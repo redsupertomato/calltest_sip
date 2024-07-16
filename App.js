f785380cdd7598c929a8652f6f216d401aa53736
msteinwinter49 <49884643+msteinwinter49@users.noreply.github.com>
2024-07-16 14:03:41 -0400

    Add Register and CALL buttons.

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, View, Text, Pressable, TextInput, Keyboard } from 'react-native';
+import 'expo-dev-client';
 
-export default function App() {
+export default function App () {
+  console.log ('>>>>> starting >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+  const [status, setStatus] = useState ('idle');
+  const [ext, setExt] = useState ('');
+
+  let userAgent;
+
+  // >>>>> Register the user.
+  const handleRegister = () => {
+    Keyboard.dismiss();
+    alert('Registered');
+    setStatus ('registering');
+    // userAgent = sipRegister ();
+  }
+  const handleCall = () => {
+    Keyboard.dismiss();
+    alert('Calling extension: ' + ext);
+    setStatus ('calling');
+    // sipCall (userAgent, ext)
+  }
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.page}>
+      <Text style={styles.text}>CallTest1</Text>
+      <Pressable onPress={ handleRegister } style={styles.button}>
+        <Text style={styles.title}>Register</Text>
+      </Pressable>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setExt(text)}
+        placeholder="extension to call"
+        keyboardType="numeric"
+      />
+      <Pressable onPress={ handleCall } style={styles.button}>
+        <Text style={styles.title}>Call</Text>
+      </Pressable>
+      <Text style={styles.text}>Status: {status}</Text>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const styles = StyleSheet.create({
+    page: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    button: {
+      marginBottom: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderColor: 'gray',
+      borderWidth: 1,
+      borderRadius: 20,
+      padding: 5, 10,
+    },
+    title: {
+      fontSize: 20,
+    },
+    input: {
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      marginBottom: 10,
+      padding: 10,
+    },
+    text: {
+      marginBottom: 20,
+      fontSize: 20,
+    },
+  });
